1fbc1517d653b8afc9823b38c8ee8beb632d98a6
Phil Sinatra <code@philsinatra.com>
2020-07-10 13:30:44 -0400

    Add Prettier Config

--- examples/04-node/02-exports/lib.js
+++ examples/04-node/02-exports/lib.js
@@ -1,5 +1,8 @@
 module.exports = {
   square: x => {
     return x * x
-  }
+  },
+  add: (x, y) => {
+    return x + y
+  },
 }
--- examples/04-node/02-exports/main.js
+++ examples/04-node/02-exports/main.js
@@ -1,3 +1,4 @@
 const lib = require('./lib')
 
 console.log(lib.square(11))
+console.log(lib.add(4, 3))
